a5645dfee694de964051c5dc68d581c21db28671
Kelly Moore <kellygarskemoore@Kellys-MacBook-Pro.local>
2015-10-30 22:45:47 -0500

    number button x value working

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -1,32 +1,71 @@
 /**
  * Created by kellygarskemoore on 10/30/15.
  */
-
+var doMathObject = {};
 
 $(document).ready(function(){
-    //perform the math operation when button clicked
+    //when number button pressed, POST that number
+    $(".calcButtonsDiv").on('click', '#one', function(){
+        postNumber(1);
+    });
+    $(".calcButtonsDiv").on('click', '#two', function(){
+        postNumber(2);
+    });
+    $(".calcButtonsDiv").on('click', '#three', function(){
+        postNumber(3);
+    });
+    $(".calcButtonsDiv").on('click', '#four', function(){
+        postNumber(4);
+    });
+    $(".calcButtonsDiv").on('click', '#five', function(){
+        postNumber(5);
+    });
+    $(".calcButtonsDiv").on('click', '#six', function(){
+        postNumber(6);
+    });
+    $(".calcButtonsDiv").on('click', '#seven', function(){
+        postNumber(7);
+    });
+    $(".calcButtonsDiv").on('click', '#eight', function(){
+        postNumber(8);
+    });
+    $(".calcButtonsDiv").on('click', '#nine', function(){
+        postNumber(9);
+    });
+    $(".calcButtonsDiv").on('click', '#zero', function(){
+        postNumber(0);
+    });
+
+    //when the operator button is clicked
     $(".buttonsDiv").on('click', '#add', function(){
-        postSendInputs("Add");
+        postMathOperator("Add");
     });
     $(".buttonsDiv").on('click', '#subtract', function(){
-        postSendInputs("Subtract");
+        postMathOperator("Subtract");
     });
     $(".buttonsDiv").on('click', '#multiply', function(){
-        postSendInputs("Multiply");
+        postMathOperator("Multiply");
     });
     $(".buttonsDiv").on('click', '#divide', function(){
-        postSendInputs("Divide");
+        postMathOperator("Divide");
     });
+
     //clear contents if clear button clicked
     $(".answerDiv").on('click', '#clearBtn', function(){
         $("#showTotalDiv").empty();
     })
-
 });
 
-//post to server to do the math calculations
-function postSendInputs(mathOperatorString){
-    var doMathObject = {};
+//functions to set up the object to send to server
+function postMathOperator(mathOperatorString){
+    doMathObject["type"] = mathOperatorString;
+    callServer();
+}
+
+function postNumber(inputNumber){
+    doMathObject["myValueX"] = inputNumber;
+    doMathObject["myValueY"] = 5;
+}
     //use the .each to put inputs into an object to send to server
                                     //myValueX myValueY
     //$.each($("#inputForm").serializeArray(), function(i, field){
@@ -34,13 +73,15 @@ function postSendInputs(mathOperatorString){
     //});
 
     //then add the button type into the object
-    doMathObject["type"] = mathOperatorString;
 
     //erase the input field
    // $("#inputForm").find("input[type=text]").val("");
 
 
     //make the call to the server "hello...is it me you're looking for?" post the object
+
+function callServer(){
+
     $.ajax({
         type: "POST",
         url: "/data",
@@ -51,5 +92,5 @@ function postSendInputs(mathOperatorString){
             $("#showTotalDiv").text(myTotal);
 
         }
-    })
+    });
 }
